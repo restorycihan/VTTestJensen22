10d47d89753ef7df832333bb1c5b440af286aaa9
restorycihan <82883230+restorycihan@users.noreply.github.com>
2022-05-03 16:12:46 +0200

    Created Accounts file.

---
+++ Account.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace In2
+{
+    public class Account
+    {
+        int accountNumber { get; set; }
+        string accountType { get; set; }
+        float balance { get; set; }
+
+    }
+}
