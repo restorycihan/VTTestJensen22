60fd1ff5df66d5d27efa506853286fe1da92e536
restorycihan <82883230+restorycihan@users.noreply.github.com>
2022-05-03 16:32:41 +0200

    Updated with functionality description.

--- Account.cs
+++ Account.cs
@@ -1,3 +1,9 @@
+/*
+Man ska kunna utföra transaktioner (insättning/uttag), hämta kontonummer, samt presentera kontot (visa kontonummer, saldo, kontotyp).
+Implementera metoder som säkerställer ovanstående krav i klassen Bank. 
+(Bank klassdesign inkluderar metoderna som ska användas. Komplettera
+dessa med en implementation eller fler metoder om det behövs.)
+*/
 
 using System;
 
@@ -5,7 +11,7 @@ namespace In2
 {
     public class Account
     {
-        int accountNumber { get; set; }
+        int accountNumber { get; set; }     // det kan inte finnas flera konton med samma kontonummer
         string accountType { get; set; }
         float balance { get; set; }
 
