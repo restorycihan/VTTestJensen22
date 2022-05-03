ac79c5219c8625b0d41c621f125fecbf134d8021
restorycihan <82883230+restorycihan@users.noreply.github.com>
2022-05-03 21:20:01 +0200

    Functionality updates.

--- Account.cs
+++ Account.cs
@@ -4,16 +4,23 @@ Implementera metoder som säkerställer ovanstående krav i klassen Bank.
 (Bank klassdesign inkluderar metoderna som ska användas. Komplettera
 dessa med en implementation eller fler metoder om det behövs.)
 */
-
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace In2
+namespace TestverktygInlämning2
 {
     public class Account
     {
-        int accountNumber { get; set; }     // det kan inte finnas flera konton med samma kontonummer
-        string accountType { get; set; }
-        float balance { get; set; }
+        public int accountNumber { get; set; }     // det kan inte finnas flera konton med samma kontonummer
+        public string accountType { get; set; }
+        public float balance { get; set; }
 
+        public override string ToString()
+        {
+            return accountNumber.ToString() + " " + accountType + " " + balance.ToString();
+        }
     }
 }
