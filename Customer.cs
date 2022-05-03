0ed83bb7014bbdac65710b677523a84e9a894194
restorycihan <82883230+restorycihan@users.noreply.github.com>
2022-05-03 16:36:09 +0200

    Update with functionality description.

--- Customer.cs
+++ Customer.cs
@@ -1,5 +1,12 @@
+/*
+
+Man ska till exempel kunna ändra kundens namn samt hämta information om kunden (personnummer, för- och efternamn samt hämta information om kundens konton (kontonummer, saldo, kontotyp)). 
+Dessutom ska man kunna hantera kundens konto(n). Implementera metoder som säkerställer ovanstående krav i klassen Bank nedan.
+(Bank inkluderar förslag på metoder. Komplettera dessa med fler metoder om det behövs).
+*/
+
 using System;
-using System.Collections.IList;
+using System.Collections.Generic;
 
 namespace In2
 {
