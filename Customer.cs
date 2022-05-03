5347479f11569ee38e8a2202c813008b06f6f01a
restorycihan <82883230+restorycihan@users.noreply.github.com>
2022-05-03 16:12:13 +0200

    Create Customer file.

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.IList;
+
+namespace In2
+{
+    public class Customer
+    {
+        int id { get; set; }
+        string firstName { get; set; }
+        string surName { get; set; }
+        string personalNumber { get; set; }
+        List<int> customerAccounts;
+
+    }
+}
