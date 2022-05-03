0e18c59f9c939c013f1e6864fd2834a27edebc7e
restorycihan <82883230+restorycihan@users.noreply.github.com>
2022-05-03 21:21:39 +0200

    Functionality update.

--- Customer.cs
+++ Customer.cs
@@ -4,19 +4,22 @@ Man ska till exempel kunna ändra kundens namn samt hämta information om kunden
 Dessutom ska man kunna hantera kundens konto(n). Implementera metoder som säkerställer ovanstående krav i klassen Bank nedan.
 (Bank inkluderar förslag på metoder. Komplettera dessa med fler metoder om det behövs).
 */
-
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace In2
+namespace TestverktygInlämning2
 {
     public class Customer
     {
-        int id { get; set; }
-        string firstName { get; set; }
-        string surName { get; set; }
-        string personalNumber { get; set; }
-        List<int> customerAccounts;
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string surName { get; set; }
+        public string personalNumber { get; set; }
 
+        public List<Account> customerAccounts;
     }
 }
+
