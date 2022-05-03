825144f17cb13aaf23de4b37c3047dd7e7cdf6a0
restorycihan <82883230+restorycihan@users.noreply.github.com>
2022-05-03 21:18:59 +0200

    Functionality updates.

--- Bank.cs
+++ Bank.cs
@@ -1,30 +1,43 @@
-/*
-Klassen Bank ska innehålla en lista med alla kunder.
-Klassen ska innehålla ett antal metoder som hanterar kunder och dess konton.
-*/
-
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using Customer;
-using Account;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace In2
+namespace TestverktygInlämning2
 {
     public class Bank
     {
         List<Customer> customerList;
 
         // Läser in text filen och befolkar listan som ska innehålla kunderna.
-        public virtual void Load()
+        public virtual void Load(string path)
         {
             customerList = new List<Customer>();
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\data.txt");
+            string[] lines = System.IO.File.ReadAllLines(path);
             List<string> list = new List<string>();
             foreach (string line in lines)
             {
                 list = line.Split(':').ToList();
-                AddCustomer(list[1], list[2]);
+
+                string personalNumber = list[2];
+                AddCustomer(list[1], personalNumber);
+                Customer customer = GetCustomer(personalNumber);
+                
+                customer.customerAccounts = new List<Account>();
+
+                int index = 3;
+                while (index < list.Count-1) {
+                    Account account = new Account()
+                    {
+                        accountNumber = int.Parse(list[index]),
+                        accountType = list[index+1],
+                        balance = float.Parse(list[index + 2], CultureInfo.InvariantCulture.NumberFormat)
+                    };
+                    customer.customerAccounts.Add(account);
+                    index += 3;
+                }
             }
         }
 
@@ -50,16 +63,16 @@ namespace In2
 
         public virtual Customer GetCustomer(string PersonalNumber)
         {
-            Customer customer = null;
-            foreach (Customer aCustomer in this.customerList)
+            Customer resultCustomer = null;
+            foreach (Customer customer in this.customerList)
             {
                 if (customer.personalNumber == PersonalNumber)
                 {
-                    customer = aCustomer;
+                    resultCustomer = customer;
                     break;
                 }
             }
-            return customer;
+            return resultCustomer;
         }
 
         /*
@@ -71,7 +84,7 @@ namespace In2
             Customer customer = GetCustomer(PersonalNumber);
             customerInfo.Add(customer.firstName);
             customerInfo.Add(customer.personalNumber);
-            foreach (int account in customer.customerAccounts)
+            foreach (Account account in customer.customerAccounts)
             {
                 customerInfo.Add(account.ToString());
             }
@@ -83,9 +96,14 @@ namespace In2
         */
         public virtual bool ChangeCustomerName(string Name, string PersonalNumber)
         {
-            bool successfull = false;
-
-            return successfull;
+            bool successful = false;
+            Customer customer = GetCustomer(PersonalNumber);
+            if (customer != null)
+            {
+                customer.firstName = Name;
+                successful = true;
+            }
+            return successful;
         }
 
         /*
@@ -93,7 +111,8 @@ namespace In2
         */
         public virtual void RemoveCustomer(string PersonalNumber)
         {
-
+            Customer customer = GetCustomer(PersonalNumber);
+            customerList.Remove(customer);
         }
 
         /*
@@ -101,19 +120,36 @@ namespace In2
         */
         public virtual int AddAccount(string PersonalNumber)
         {
+            Customer customer = GetCustomer(PersonalNumber);
+
             int accountCode = -1;
             if (PersonalNumber != "")
             {
                 Random rnd = new Random();
                 accountCode = rnd.Next(1000, 1999);
+                customer.customerAccounts.Add(new Account() { accountNumber = accountCode, accountType = "debit", balance = 0});
             }
             return accountCode;
         }
 
         public virtual Account GetAccount(string PersonalNumber, int AccountId)
         {
-            string accountDetails = "";
-            return accountDetails;
+            Customer customer = GetCustomer(PersonalNumber);
+            Account gottenAccount = null;
+
+            if (customer != null)
+            {
+                foreach (Account account in customer.customerAccounts)
+                {
+                    if (account.accountNumber == AccountId)
+                    {
+                        gottenAccount = account;
+                        break;
+                    }
+                }
+            }
+
+            return gottenAccount;
         }
 
         /*
@@ -128,7 +164,13 @@ namespace In2
         // Gör en insättning på kontot, returnerar true om det gick bra annars false.
         public virtual bool Deposit(string PersonalNumber, int AccountId, float Amount)
         {
+            Account account = GetAccount(PersonalNumber, AccountId);
             bool successfull = false;
+            if (account != null)
+            {
+                account.balance += Amount;
+                successfull = true;
+            }
             return successfull;
         }
 
